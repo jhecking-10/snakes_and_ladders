959d7135e9bbba702d1bfab14516029e886966d7
jhecking-10 <jhecking_10@protonmail.com>
2026-08-30 23:20:41 +0900

    Update game.py

--- game.py
+++ game.py
@@ -5,15 +5,24 @@ def create_game_board():
     return list(range(1, 101))
 
 board = create_game_board()
-starting_position = board[0]
+starting_space = board[0]
+starting_turn = 1
 
 def roll_die():
     _ = input("Press enter to roll the die")
     return random.randint(1, 6)
 
-def player_turn(current_space: int):
+def player_turn(space: int, turn: int):
     roll = roll_die()
-    print(f"You rolled a {roll} and are now on space {current_space + roll}")
+    turn += 1
+    space += roll
+    return space, turn, roll
 
-def determine_current_space():
-    pass
+def game():
+    current_space, current_turn, last_roll = player_turn(starting_space, starting_turn)
+    print(f"You rolled a {last_roll} and are now on space {current_space}. Your next turn will be #{current_turn}")
+    while current_space < 100:
+        current_space, current_turn, last_roll = player_turn(current_space, current_turn)
+        print(f"You rolled a {last_roll} and are now on space {current_space}. Your next turn will be #{current_turn}")
+
+game()
