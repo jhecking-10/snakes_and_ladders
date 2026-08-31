a4701b0169af4d4c115d35e85382982ad03e0061
James Hecking <jhecking_10@protonmail.com>
2026-08-31 22:32:48 +0900

    Add comments game.py

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import random
+import random # for random die roll
 
 
 def create_game_board():
@@ -7,6 +7,7 @@ def create_game_board():
 board = create_game_board()
 
 def roll_die():
+    # assign input to underscore because no value is required
     _ = input("Press enter to roll the die")
     return random.randint(1, 6)
 
@@ -17,8 +18,10 @@ def player_turn(space: int, turn: int):
     return space, turn, roll
 
 def game():
+    # starts player on space 1 and initializes their first turn
     current_space, current_turn, last_roll = player_turn(board[0], 1)
     print(f"You rolled a {last_roll} and are now on space {current_space}. Your next turn will be #{current_turn}")
+    # possibly need to update below logic to utilize board; also may recreate the board as an object in classes.py
     while current_space < 100:
         if current_space >= 94:
             print("Roll precisely... Don't go past 100")
